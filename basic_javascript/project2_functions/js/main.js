a2984b37fcc3996fd09a0e16a2f255407ee3fea9
vizorius <vmvidmantas@gmail.com>
2025-09-08 21:34:22 +0100

    added funtion code to html/css

---
+++ basic_javascript/project2_functions/js/main.js
@@ -0,0 +1,43 @@
+// Function that concatenates two variables and displays the result in the paragraph with id "function"
+function showCustomMessage() {
+	// First part of the message
+	var greeting = "Welcome to ";
+	// Second part of the message
+	var project = "JavaScript Functions!";
+	// Concatenate the two strings
+	var result = greeting + project;
+	// Display the result in the paragraph with id "function"
+	document.getElementById("function").innerHTML = result;
+}
+
+// Function that demonstrates the use of the += operator for string concatenation
+function myFunction() {
+	// Start with an initial sentence
+	var sentence = "I am learning";
+	// Concatenate more text using +=
+	sentence += " a lot from this book!";
+	// Display the concatenated sentence in the paragraph with id "Concatenate"
+	document.getElementById("Concatenate").innerHTML = sentence;
+}
+
+// Function that concatenates two variables and displays the result in the paragraph with id "function"
+function showCustomMessage() {
+	// First part of the message
+	var greeting = "Welcome to ";
+	// Second part of the message
+	var project = "JavaScript Functions!";
+	// Concatenate the two strings
+	var result = greeting + project;
+	// Display the result in the paragraph with id "function"
+	document.getElementById("function").innerHTML = result;
+}
+
+// Function that demonstrates the use of the += operator for string concatenation
+function myFunction() {
+	// Start with an initial sentence
+	var sentence = "I am learning";
+	// Concatenate more text using +=
+	sentence += " a lot from this book!";
+	// Display the concatenated sentence in the paragraph with id "Concatenate"
+	document.getElementById("Concatenate").innerHTML = sentence;
+}
