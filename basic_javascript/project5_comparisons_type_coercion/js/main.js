35be04cf2dc08f00ea4b975db95671ed05c6b187
vizorius <vmvidmantas@gmail.com>
2025-09-15 23:01:27 +0100

    worked on 'typeof'

---
+++ basic_javascript/project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,8 @@
+document.writeln(typeof "word");
+document.writeln(typeof 3);
+document.writeln(typeof"10" + 5);
+
+document.writeln("10" + 5);
+document.writeln("10" - 5);
+document.writeln("10" * 5);
+document.writeln("10" / 5);
